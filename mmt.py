dc415f502d111ebb36f06bafd534e601a2b07fa8
feib_lab <frank.feibao@gmail.com>
2022-10-04 12:37:22 +0900

    add model's skeleton code

---
+++ mmt.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+from torch.nn import Transformer
+from transformers import BertModel, AutoTokenizer
+
+
+class MMT(nn.Module):
+    def __int__(self):
+        super(MMT, self).__init__()
+
+    def forward(self):
+        return None
